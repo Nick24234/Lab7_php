8c7914dca94ac73c83067f5a160ef4391beb0b2a
Nick24234 <114647778+Nick24234@users.noreply.github.com>
2025-12-09 23:58:07 +0200

    Add files via upload

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative "boot"
+
+require "rails/all"
+
+
+Bundler.require(*Rails.groups)
+
+module Blog
+  class Application < Rails::Application
+   
+    config.load_defaults 7.0
+
+  
+  end
+end
+
